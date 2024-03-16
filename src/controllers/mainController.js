83146abeb5c61eceaf07c4d330907d4facfec6be
toniquealbornoz <toniquealbornoz@yahoo.com.ar>
2024-03-16 19:05:18 -0300

    agregando funcionalidad a la barra de búsqueda

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-// const productsFilePath = path.join(__dirname, "../data/products.json");
-// const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const controller = {
   index: (req, res) => {
     res.render("index.ejs", { pageTitle: "Página de Incio" });
   },
-};
+  search: (req, res) => {
+    let productSearch = req.query.search.toUpperCase();
+    let results = products.filter(product => {
+			return product.nombre.toUpperCase().includes(productSearch)
+		})
+    res.render("search.ejs", {results, pageTitle: "Página de Incio"})
+}}
 
 module.exports = controller;
